84ccaf3c05e9f137ba5c70c2fe599cd8243223cf
Aye Chan Aung Thwin <ayechanaungthwin@gmail.com>
2025-09-17 21:11:26 +0700

    minimized

---
+++ README.md
@@ -0,0 +1,7 @@
+# Thai PromptPay QR Code Generator
+
+## How it looks like
+Run it by pressing [here](https://ayechanaungthwin.github.io/prompt-pay-qr-code-generator).
+
+## Electronics Engineer-cum-J2EE Backend Developer ##
+-  Created by - Aye Chan Aung Thwin
---
+++ js/index.js
@@ -0,0 +1,127 @@
+// Created by - Aye Chan Aung Thwin
+
+function _0x509c(_0x2251fd, _0x531a74) {
+    const _0x5dd1e8 = _0x5dd1();
+    return _0x509c = function(_0x509c19, _0x320705) {
+        _0x509c19 = _0x509c19 - 0x14a;
+        let _0x23f11d = _0x5dd1e8[_0x509c19];
+        return _0x23f11d;
+    }
+    ,
+    _0x509c(_0x2251fd, _0x531a74);
+}
+const _0x4f6842 = _0x509c;
+(function(_0x1319f5, _0x2ed9db) {
+    const _0x1917e6 = _0x509c
+      , _0x206112 = _0x1319f5();
+    while (!![]) {
+        try {
+            const _0x2bef7a = parseInt(_0x1917e6(0x15d)) / 0x1 + -parseInt(_0x1917e6(0x15c)) / 0x2 + parseInt(_0x1917e6(0x160)) / 0x3 * (parseInt(_0x1917e6(0x16d)) / 0x4) + parseInt(_0x1917e6(0x14e)) / 0x5 * (-parseInt(_0x1917e6(0x165)) / 0x6) + parseInt(_0x1917e6(0x16b)) / 0x7 + -parseInt(_0x1917e6(0x152)) / 0x8 + parseInt(_0x1917e6(0x159)) / 0x9 * (parseInt(_0x1917e6(0x177)) / 0xa);
+            if (_0x2bef7a === _0x2ed9db)
+                break;
+            else
+                _0x206112['push'](_0x206112['shift']());
+        } catch (_0x305d3a) {
+            _0x206112['push'](_0x206112['shift']());
+        }
+    }
+}(_0x5dd1, 0x76a27));
+function _0x5dd1() {
+    const _0x157acc = ['innerHTML', 'test', 'querySelector', 'toCanvas', 'click', 'replace', '23245KidroN', 'error', 'amount', '#qrContainer\x20canvas', '203760bNTFjN', 'value', 'DOMContentLoaded', 'removeChild', 'canvas', 'createElement', 'keypress', '6444CFWIEb', 'THB', 'href', '1603386kLwwXc', '383203iZRGrl', 'Please\x20generate\x20a\x20QR\x20code\x20first', 'download', '713127wXEmMw', 'addEventListener', 'input[type=\x22text\x22]', 'ThaiPromptPayQR', 'image/png', '324UHIetc', 'disabled', 'querySelectorAll', 'Please\x20enter\x20at\x20least\x20a\x20phone\x20number\x20with/without\x20amount', 'Leave\x20Blank\x20or\x20100\x20or\x2050.25)', 'phone', '6537412CdLzBu', 'Error\x20generating\x20QR\x20code', '4GXEjky', 'appendChild', 'trim', 'downloadBtn', 'forEach', 'Please\x20enter\x20a\x20valid\x20amount\x20(e.g.,\x20100\x20or\x2050.25)', 'No\x20QR\x20code\x20available\x20to\x20download', '<div\x20class=\x22img-placeholder\x22>Error\x20generating\x20QR\x20code</div>', 'body', 'getElementById', '130ABetix', 'Please\x20enter\x20a\x20valid\x20phone\x20number'];
+    _0x5dd1 = function() {
+        return _0x157acc;
+    }
+    ;
+    return _0x5dd1();
+}
+let _0x85diemgi91a = ![];
+function _0x8ieumx2a9() {
+    const _0xf13f27 = _0x509c
+      , _0x40627d = document[_0xf13f27(0x176)](_0xf13f27(0x16a))[_0xf13f27(0x153)]
+      , _0x48fe2d = document[_0xf13f27(0x176)]('amount')[_0xf13f27(0x153)];
+    if (!_0x40627d) {
+        alert(_0xf13f27(0x168));
+        return;
+    }
+    const _0x1209a3 = /^(\+?[\d\s\-\(\)]{10,})$/;
+    if (!_0x1209a3[_0xf13f27(0x17a)](_0x40627d)) {
+        alert(_0xf13f27(0x178));
+        return;
+    }
+    const _0x2b4241 = _0x8deugjei937(_0x48fe2d);
+    if (!_0x2b4241) {
+        alert(_0xf13f27(0x169));
+        return;
+    }
+    var _0x55e2f9 = !![];
+    !_0x48fe2d && (_0x55e2f9 = ![]);
+    const _0x4f25bb = _0x34uebg(_0x40627d, _0x48fe2d, _0x55e2f9)
+      , _0x4a8071 = document[_0xf13f27(0x176)]('qrContainer');
+    _0x4a8071[_0xf13f27(0x179)] = '';
+    const _0x2981ec = document['createElement'](_0xf13f27(0x156));
+    _0x4a8071[_0xf13f27(0x16e)](_0x2981ec),
+    QRCode[_0xf13f27(0x14b)](_0x2981ec, _0x4f25bb, {
+        'width': 0xb4,
+        'margin': 0x1,
+        'color': {
+            'dark': '#333',
+            'light': '#ffffff'
+        }
+    }, function(_0x1df428) {
+        const _0xe9d979 = _0xf13f27;
+        _0x1df428 ? (console[_0xe9d979(0x14f)](_0x1df428),
+        alert(_0xe9d979(0x16c)),
+        _0x4a8071[_0xe9d979(0x179)] = _0xe9d979(0x174),
+        _0x85diemgi91a = ![],
+        document['getElementById'](_0xe9d979(0x170))[_0xe9d979(0x166)] = !![]) : (_0x85diemgi91a = !![],
+        document[_0xe9d979(0x176)](_0xe9d979(0x170))[_0xe9d979(0x166)] = ![]);
+    });
+}
+function _0x8deugjei937(_0x49b44b) {
+    const _0x348d5d = _0x509c;
+    if (!_0x49b44b || _0x49b44b[_0x348d5d(0x16f)]() === '')
+        return !![];
+    const _0x3b6222 = /^\d+(\.\d{1,2})?$/;
+    if (!_0x3b6222[_0x348d5d(0x17a)](_0x49b44b))
+        return alert(_0x348d5d(0x172)),
+        ![];
+    return !![];
+}
+function _0x7aeudbg86() {
+    const _0x6a141b = _0x509c
+      , _0x2e38fd = document['getElementById']('name')[_0x6a141b(0x153)]
+      , _0x566af4 = document[_0x6a141b(0x176)]('phone')[_0x6a141b(0x153)]
+      , _0x13e971 = document[_0x6a141b(0x176)](_0x6a141b(0x150))[_0x6a141b(0x153)];
+    if (!_0x85diemgi91a) {
+        alert(_0x6a141b(0x15e));
+        return;
+    }
+    const _0x15eaa3 = document[_0x6a141b(0x14a)](_0x6a141b(0x151));
+    if (!_0x15eaa3) {
+        alert(_0x6a141b(0x173));
+        return;
+    }
+    const _0x7f4148 = document[_0x6a141b(0x157)]('a');
+    let _0x3c7040 = _0x6a141b(0x163) + _0x2e38fd[_0x6a141b(0x14d)](/\s/g, '');
+    if (_0x2e38fd)
+        _0x3c7040 += '' + _0x566af4;
+    if (_0x13e971)
+        _0x3c7040 += _0x6a141b(0x15a) + _0x13e971;
+    _0x3c7040 += '.png',
+    _0x7f4148[_0x6a141b(0x15f)] = _0x3c7040,
+    _0x7f4148[_0x6a141b(0x15b)] = _0x15eaa3['toDataURL'](_0x6a141b(0x164)),
+    document[_0x6a141b(0x175)]['appendChild'](_0x7f4148),
+    _0x7f4148[_0x6a141b(0x14c)](),
+    document[_0x6a141b(0x175)][_0x6a141b(0x155)](_0x7f4148);
+}
+document[_0x4f6842(0x161)](_0x4f6842(0x154), function() {
+    const _0x5438dd = _0x4f6842
+      , _0x2192f1 = document[_0x5438dd(0x167)](_0x5438dd(0x162));
+    _0x2192f1[_0x5438dd(0x171)](_0x1e6cc9 => {
+        const _0x88c3ae = _0x5438dd;
+        _0x1e6cc9[_0x88c3ae(0x161)](_0x88c3ae(0x158), function(_0x4a5161) {
+            _0x4a5161['key'] === 'Enter' && _0x8ieumx2a9();
+        });
+    }
+    );
+});
